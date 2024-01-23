7bf1c34e578c43d81527583b71ed5f40bc520bad
Dũng Nguyễn Quốc <nguyenquocdung26032003@gmail.com>
2024-01-23 09:51:03 +0700

    23/1/24 9:51 AM

---
+++ scr/Screens/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+const Home = () => {
+    return (
+        <View>
+            <Text>DMQTa</Text>
+            <Text><Icon name='rocket' size={30} color="#900"/></Text>
+        </View>
+    )
+}
+
+export default Home
