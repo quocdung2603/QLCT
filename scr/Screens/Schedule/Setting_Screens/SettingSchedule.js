84f5df93f667dc5975ce0a9dc40fb2c1dcccb576
Dũng Nguyễn Quốc <nguyenquocdung26032003@gmail.com>
2024-03-06 10:20:21 +0700

    6/3/24 10:21 AM

--- scr/Screens/Schedule/Setting_Screens/SettingSchedule.js
+++ scr/Screens/Schedule/Setting_Screens/SettingSchedule.js
@@ -17,12 +17,9 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome'
 const SettingSchedule = () =>{
     const navigation = useNavigation();
     return (    
-        <View style={{flex:1, flexDirection:'column', backgroundColor:'white',}}>
+        <View style={{flex:1, flexDirection:'column', backgroundColor:'white', padding:10}}>
             <View style={{ flexDirection: 'row', margin: 10, justifyContent: 'center', alignContent: 'center', margin:10 }}>
-                <View style={{ marginRight: 'auto' }}>
-                    <AntDesign name='arrowleft' size={30} color='#000' />
-                </View>
-                <Text style={{ color: '#000', fontSize: 20, marginEnd: 'auto', fontWeight:'bold' }}>Setting</Text>
+                <Text style={{ color: '#000', fontSize: 20, fontWeight:'bold' }}>Setting</Text>
             </View>
             <View style={{marginHorizontal:10, marginVertical:12, flexDirection:'row', justifyContent:'center', alignContent:'center'}}>
                 <Text style={{fontSize:20, marginEnd:'auto', color:'#000'}}>Currency</Text>
