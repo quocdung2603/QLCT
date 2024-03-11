7f659cc2341e9c98cb7ff6349a7eba1035145475
Dũng Nguyễn Quốc <nguyenquocdung26032003@gmail.com>
2024-03-11 22:55:23 +0700

    11/3/24 xong EditDetailTransaction

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ import {name as appName} from './app.json';
 import App from './scr/Navigation/App';
 import test from './scr/Screens/Test';
 import EditNote from './scr/Screens/Schedule/Note_Screens/EditNote';
-import DetailTransaction from './scr/Screens/Expense/Transaction_Screen/DetailTransaction';
-AppRegistry.registerComponent(appName, () => DetailTransaction);
+import EditDetailTransaction from './scr/Screens/Expense/Transaction_Screen/EditDetailTransaction';
+AppRegistry.registerComponent(appName, () => App);
