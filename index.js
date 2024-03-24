ddbf68fad57a010e382c405d2ca2d0ad052676ff
Dũng Nguyễn Quốc <nguyenquocdung26032003@gmail.com>
2024-03-24 22:30:47 +0700

    24/3/24 10:30PM

--- index.js
+++ index.js
@@ -6,9 +6,4 @@ import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 
 import App from './scr/Navigation/App';
-import test from './scr/Screens/Test';
-import EditNote from './scr/Screens/Schedule/Note_Screens/EditNote';
-import DetailTransaction from './scr/Screens/Expense/Transaction_Screen/DetailTransaction';
-import EditDetailTransaction from './scr/Screens/Expense/Transaction_Screen/EditDetailTransaction';
-import Income from './scr/Screens/Expense/Home_Screen/Income';
 AppRegistry.registerComponent(appName, () => App);
