23798fbd0921176f1563276591594c341f247305
Dũng Nguyễn Quốc <nguyenquocdung26032003@gmail.com>
2024-04-04 16:15:07 +0700

    abcde

---
+++ scr/Screens/Health/Excersise_Screen/ExcersiseMain.js
@@ -0,0 +1,260 @@
+import React, { useContext, useEffect, useState } from 'react';
+import {
+    Alert,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    Image,
+    TextInput,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
+import Octicons from 'react-native-vector-icons/Octicons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Home from '../../Expense/Home_Screen/Home';
+//
+import { CheckBox } from '@rneui/base';
+import { Dropdown } from 'react-native-element-dropdown';
+const dataEx = [
+    { label: 'Tập ngực', value: 1 },
+    { label: 'Tập tay', value: 2 },
+    { label: 'Tập bụng', value: 3 },
+    { label: 'Tập chân', value: 4 },
+];
+
+const ExcersiseMain = ({navigation}) => {
+    const [SE, setSE] = useState(0)
+    const [isChecked, setIsChecked] = useState(false);
+    const handleCheckboxChange = () => {
+        setIsChecked(!isChecked);
+    };
+
+    const [PlanExx, setPlanExx] = useState("")
+    const renderItem = item => {
+        return (
+            <View style={styles.item}>
+                <Text style={styles.textItem}>{item.label}</Text>
+                {item.value === PlanExx && (
+                    <AntDesign
+                        style={styles.icon}
+                        color="black"
+                        name="Safety"
+                        size={20}
+                    />
+                )}
+            </View>
+        );
+    };
+    const handleStr = async ()=>{
+        try {
+            const item = {
+                PlanExx: PlanExx
+            }
+            setPlanExx("")
+            navigation.navigate('Home');
+        } catch (error) {
+            Alert.alert("lỗi");
+            console.log(error);
+        }
+    }
+    return(
+        <View style={{flex:1, flexDirection:'column', backgroundColor:'#fff'}}>
+            <View style={{ flexDirection: 'row', margin: 10, alignItems: 'center' }}>
+                <TouchableOpacity
+                    onPress={() => navigation.openDrawer()}
+                    style={{ justifyContent: 'center', alignItems: 'center', marginRight: 'auto' }}>
+                    <Octicons name='three-bars' size={30} color='#000' />
+                </TouchableOpacity>
+            </View>
+            <View style={{flexDirection:'row', alignItems:'center', margin:10}}>
+                <Dropdown
+                    style={styles.dropdown}
+                    placeholderStyle={styles.placeholderStyle}
+                    selectedTextStyle={styles.selectedTextStyle}
+                    inputSearchStyle={styles.inputSearchStyle}
+                    iconStyle={styles.iconStyle}
+                    data={dataEx}
+                    search
+                    maxHeight={300}
+                    labelField="label"
+                    valueField="value"
+                    placeholder="Select item"
+                    searchPlaceholder="Search..."
+                    value={PlanExx}
+                    onChange={item => {
+                        setPlanExx(item.value);
+                    }}
+                    renderLeftIcon={() => (
+                        <AntDesign style={styles.icon} color="black" name="Safety" size={20} />
+                    )}
+                    renderItem={renderItem}
+                />
+                <TouchableOpacity
+                    onPress={() => { SE === 0 ? setSE(1) : setSE(0)}}
+                    style={{marginStart:'auto', marginEnd:10,padding:5, borderWidth:1, borderRadius:10}}>
+                    {SE === 0 ? (
+                        <Text style={{fontSize:20}}>Bắt đầu</Text>
+                    ) : (
+                        <Text style={{fontSize:20}}>Kết thúc</Text>
+                    )}
+                </TouchableOpacity>
+            </View> 
+            
+            <View style={{ flexDirection: 'column', margin: 10 }}>
+                <Text style={{fontSize:18, fontWeight:'bold', textAlign:'center'}}>Tổng thời gian tập luyện</Text>
+                <Text style={{fontSize:20, fontWeight:'bold', textAlign:'center'}}>00:00</Text>
+            </View>
+
+            <View style={{ flexDirection: 'column', margin: 10 }}>
+                <Text style={{fontSize:18, fontWeight:'bold', textAlign:'center'}}>Tổng số calo tiêu hao</Text>
+                <Text style={{fontSize:20, fontWeight:'bold', textAlign:'center'}}>320</Text>
+            </View>        
+            <View style={{ flexDirection: 'column', marginStart:10}}>
+                <Text style={{fontSize:18, margin:10, fontWeight:'bold'}}>Danh sách bài tập</Text>
+                {/* bt 1 */}
+                <View style={{flexDirection:'row', alignItems:'center'}}>
+                    <TouchableOpacity
+                        onPress={() => { navigation.navigate('ExcersiseItem') }}
+                        style={{ margin:10, marginEnd:'auto', flexDirection: 'row', borderWidth: 1, borderRadius: 10, padding: 10, alignItems: 'center' }}>
+                        <View style={{marginEnd:'auto', width:250}}>
+                            <Text style={{ fontSize: 18, color: '#000', fontWeight: 'bold' }}>Nghiêng vai với tạ đơn</Text>
+                            <Text style={{ fontSize: 15, color: '#000' }}>15 lần / hiệp</Text>
+                        </View>
+                        <View style={{ marginStart:'auto', backgroundColor: 'yellow' }}>
+                            <Text>Hình ảnh</Text>
+                            <Text>Hình ảnh</Text>
+                            <Text>Hình ảnh</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <CheckBox value={isChecked} onValueChange={handleCheckboxChange} />
+                </View>
+                {/* bt2 */}
+                <View style={{flexDirection:'row', alignItems:'center'}}>
+                    <TouchableOpacity
+                        onPress={() => { navigation.navigate('ExcersiseItem') }}
+                        style={{ margin:10, marginEnd:'auto', flexDirection: 'row', borderWidth: 1, borderRadius: 10, padding: 10, alignItems: 'center' }}>
+                        <View style={{marginEnd:'auto', width:250}}>
+                            <Text style={{ fontSize: 18, color: '#000', fontWeight: 'bold' }}>Nghiêng vai với tạ đơn</Text>
+                            <Text style={{ fontSize: 15, color: '#000' }}>15 lần / hiệp</Text>
+                        </View>
+                        <View style={{ marginStart:'auto', backgroundColor: 'yellow' }}>
+                            <Text>Hình ảnh</Text>
+                            <Text>Hình ảnh</Text>
+                            <Text>Hình ảnh</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <CheckBox value={isChecked} onValueChange={handleCheckboxChange} />
+                </View>
+                
+            </View>
+        </View>
+    )
+}
+
+export default ExcersiseMain;
+const styles = StyleSheet.create({
+    contentView: {
+        width: 320,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        marginTop: 20
+    },
+    circle: {
+        width: 50,
+        height: 30,
+        borderRadius: 30,
+        backgroundColor: '#7F3DFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 4
+    },
+    number: {
+        color: 'white',
+        fontSize: 12,
+        textAlign: 'center'
+    },
+    dropdown: {
+        margin: 10,
+        height: 50,
+        backgroundColor: 'white',
+        borderRadius: 12,
+        padding: 12,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
+        elevation: 2,
+        width:250
+    },
+    icon: {
+        marginRight: 5,
+    },
+    item: {
+        padding: 17,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    textItem: {
+        flex: 1,
+        fontSize: 16,
+    },
+    placeholderStyle: {
+        fontSize: 16,
+    },
+    selectedTextStyle: {
+        fontSize: 16,
+    },
+    iconStyle: {
+        width: 20,
+        height: 20,
+    },
+    inputSearchStyle: {
+        height: 40,
+        fontSize: 16,
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    touchable: {
+        borderWidth: 1,
+        borderRadius: 10,
+        marginLeft: 10,
+        padding: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 15,
+        marginRight: 15,
+    },
+    icon: {
+        marginLeft: 15,
+    },
+    modalBackground: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalContent: {
+        backgroundColor: '#fff',
+        padding: 20,
+        borderRadius: 10,
+        width:400,
+        height:400,
+    },
+});
---
+++ scr/Screens/Health/Plan_Screen/CreatePlan.js
@@ -0,0 +1,109 @@
+import React, { useContext, useEffect, useState } from 'react';
+import {
+    Alert,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    Image,
+    TextInput,
+    Modal
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
+import Octicons from 'react-native-vector-icons/Octicons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Home from '../../Expense/Home_Screen/Home';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+const CreatePlan = ({ navigation }) => {
+    const [MThemBT, setMThemBT] = useState(false)
+    return (
+        <View style={{ flex: 1, flexDirection: 'column', backgroundColor: '#fff' }}>
+            <View style={{ flexDirection: 'row', margin: 10, alignItems: 'center' }}>
+                <TouchableOpacity style={{ marginRight: 'auto' }} onPress={() => { navigation.goBack() }}>
+                    <AntDesign name='arrowleft' size={30} color='#000' />
+                </TouchableOpacity>
+                <Text style={{fontSize:22, fontWeight:'bold', color:'#000'}}>Tập Vai</Text>
+                <TouchableOpacity 
+                    style={{ marginStart: 'auto' }} 
+                    onPress={() => { 
+                    // bấm nút này sẽ lưu lại cái danh sách bài tập 
+                }}>
+                    <AntDesign name='check' size={30} color='#000' />
+                </TouchableOpacity>
+            </View>
+            <Text style={{margin:10, fontSize:20, fontWeight:'bold', color:'#000'}}>Danh sách bài tập</Text>
+            <ScrollView style={{ flex: 1, flexDirection: 'column', backgroundColor: '#fff' }}>
+                <View style={{flexDirection:'row', alignItems:'center'}}>
+                    <Text style={{fontSize:30, fontWeight:'bold', marginEnd:'auto', marginStart:10}}>1.</Text>
+                    <View style={{flexDirection:'column', width:320}} >
+                        <TouchableOpacity
+                            onPress={() => { navigation.navigate('ExcersiseItem') }}
+                            style={{marginVertical: 4, flexDirection: 'row', borderWidth: 1, borderRadius: 10, padding: 10, alignItems: 'center'}}>
+                            <Text style={{ fontSize: 20, color: '#000', fontWeight: 'bold' }}>Nghiêng vai với tạ đơn</Text>
+                            <View style={{ marginStart: 'auto', backgroundColor: 'yellow' }}>
+                                <Text>Hình ảnh</Text>
+                                <Text>Hình ảnh</Text>
+                                <Text>Hình ảnh</Text>
+                            </View>
+                        </TouchableOpacity>
+                        <View style={{flexDirection:'row', alignItems:'center', marginVertical:5, padding:5, borderWidth:1, borderRadius:10}}>
+                            <View style={{flexDirection:'row', alignItems:'center', marginEnd:'auto'}}>
+                                <Text style={{fontSize:18, fontWeight:'bold'}}>Số lần</Text>
+                                <TextInput keyboardType='number-pad' placeholder='0' style={{marginStart:10, borderWidth:1, maxHeight:40, width:60, textAlign:'center'}} />
+                            </View>
+                            <View style={{flexDirection:'row', alignItems:'center', marginStart:'auto'}}>
+                                <Text style={{fontSize:18, fontWeight:'bold'}}>Số hiệp</Text>
+                                <TextInput keyboardType='number-pad' placeholder='0' style={{marginStart:10, borderWidth:1, maxHeight:40, width:60, textAlign:'center'}} />
+                            </View>
+                        </View>     
+                    </View>
+                    <TouchableOpacity style={{marginEnd:10, marginStart:'auto'}} onPress={() => {
+                        //bấm nút này sẽ xóa cái bài tập này ra khỏi danh sách
+                    }}>
+                        <AntDesign name='delete' size={30} color='#000' />
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+            <TouchableOpacity 
+                onPress={() => {setMThemBT(true)}}
+                style={{margin:10, flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+                <Text style={{fontSize:25, fontWeight:'bold', borderWidth:1, borderRadius:10, padding:5}}>Thêm bài tập</Text>
+            </TouchableOpacity>
+            <Modal animationType='slide' transparent={true} visible={MThemBT} onRequestClose={() => { setMThemBT(false) }} >
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+                    <View style={{flexDirection:'column', backgroundColor: 'white', width: 400, height: 500, borderRadius: 10,  padding: 20 }}>
+                        <TouchableOpacity onPress={() => {setMThemBT(false)}} style={{flexDirection:'row', alignItems:'center', marginBottom:10}}>
+                            <Text style={{marginStart:'auto', fontSize:20, fontWeight:'bold', color:'#000'}}>Thêm bài tập</Text>
+                            <AntDesign name="close" size={30} color='#000' style={{marginStart:'auto'}} />
+                        </TouchableOpacity>
+                        <View style={{flexDirection:'row',alignItems:'center', margin:10, borderWidth:1, borderRadius:10, paddingHorizontal:5}}>
+                            <TextInput keyboardType='text' placeholder='Tìm kiếm bài tập ...'  style={{maxHeight:40, marginEnd:'auto'}} />
+                            <AntDesign name="search1" size={30} color='#000' style={{marginStart:'auto'}} />
+                        </View>
+                        <TouchableOpacity
+                            onPress={() => { 
+                                //thêm bài tập vào danh sách
+                            }}
+                            style={{marginVertical: 4, flexDirection: 'row', borderWidth: 1, borderRadius: 10, padding: 10, alignItems: 'center'}}>
+                            <Text style={{ fontSize: 20, color: '#000', fontWeight: 'bold' }}>Nghiêng vai với tạ đơn</Text>
+                            <View style={{ marginStart: 'auto', backgroundColor: 'yellow' }}>
+                                <Text>Hình ảnh</Text>
+                                <Text>Hình ảnh</Text>
+                                <Text>Hình ảnh</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+        </View>
+    )
+}
+export default CreatePlan;
